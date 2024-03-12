a5176a260b5612aa7e035444147f1f5eeb8118ca
王冠骁 <1156410455@qq.com>
2024-03-12 16:48:23 +0800

    新建了三个文件: print_element.py:输出元素信息. search_baidu.py:百度搜索 youdao_translate.py:翻译

---
+++ print_element.py
@@ -0,0 +1,11 @@
+def print_element(input):# 输入一个查找返回的remote webelement,输出元素的tagname,text,以及全部属性.
+
+    attributes = input.get_property("attributes")
+    print("tagname:",input.tag_name)
+    print("text:",input.text)
+    # try:
+    #     print("text:",input.text)
+    # except:
+    #     print("text:","none")
+    for attribute in attributes:
+        print(attribute['name'], ":", attribute['value'])
