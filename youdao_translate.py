a92121354770ddeae1e637f87a67de5dd731fb9f
王冠骁 <1156410455@qq.com>
2024-03-12 16:48:23 +0800

    新建了三个文件: print_element.py:输出元素信息. search_baidu.py:百度搜索 youdao_translate.py:翻译

--- youdao_translate.py
+++ youdao_translate.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import print_element
+from print_element import print_element
 from selenium.webdriver.common.keys import Keys
-import time
+from time import sleep
 import sys
 
 DEBUG=0
@@ -12,7 +12,7 @@ def youdao_translate(str):
 
     # 加载有道翻译页面
     driver.get("https://fanyi.youdao.com/")
-    time.sleep(1)
+    sleep(1)
     # 点击关闭广告
     try:
         ad_close_button = driver.find_element(By.XPATH, "/html/body/div[5]/div/img[2]")
@@ -29,7 +29,7 @@ def youdao_translate(str):
         # print("异常类型:", exc_type)
         print(exc_value)
         # print("回溯对象:", exc_traceback)
-        time.sleep(100)
+        sleep(100)
         sys.exit(1)
 
     # 选择输入框并输入"hello"
@@ -37,12 +37,12 @@ def youdao_translate(str):
     input_div.click()
     input_div.send_keys(str)
     if DEBUG:
-        print_element.print_element(input_div)
-    time.sleep(5)
+        print_element(input_div)
+    sleep(5)
     output_div = driver.find_element(By.XPATH,
                                      "/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/div[3]/div[2]/div[1]")
     if DEBUG:
-        print_element.print_element(output_div)
+        print_element(output_div)
     output_text=output_div.text
     print(output_text)
     # time.sleep(100)
